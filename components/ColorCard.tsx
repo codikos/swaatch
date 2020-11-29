5c1b51d423873359ecc79aadc1b69e4d42d4fb28
codikos <joel.matelli@codikos.io>
2020-11-29 22:30:33 +0100

    fix: better UI and no infinite loop in contrast generation

--- components/ColorCard.tsx
+++ components/ColorCard.tsx
@@ -6,7 +6,7 @@ type ColorCardProps = {
 export default function ColorCard({ color, name }: ColorCardProps) {
   return (
     <div className="mx-2 border border-gray-200 dark:border-gray-600 rounded overflow-hidden">
-      <div className="w-28 h-28" style={{ backgroundColor: `#${color}` }}></div>
+      <div className="w-full h-16" style={{ backgroundColor: `#${color}` }}></div>
       <div className="bg-white dark:bg-gray-800 px-4 py-2 text-center">
         <div className="text-gray-500 dark:text-gray-400">{name}</div>
         <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 export default function IndexPage() {
   return (
-    <div>
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">Swaatch!</h1>
-        <h2 className="text-2xl mt-5 text-center text-gray-400 dark:text-gray-300">
-          Generate accessible color palettes easily.
-        </h2>
-      </div>
-      <div className="flex flex-row justify-center">
+    <div className="flex flex-col place-content-center h-screen">
+      <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">Swaatch!</h1>
+      <h2 className="text-2xl mt-5 text-center text-gray-400 dark:text-gray-300">
+        Generate accessible color palettes easily.
+      </h2>
+      <div className="flex flex-row place-content-center my-10">
         <a href="/grays" className="btn-blue">
           Let's begin!
         </a>
