96e1b5b2403be2aa0cdea6888e647f8bb315ab12
codikos <joel.matelli@codikos.io>
2020-11-30 02:16:32 +0100

    fix: better navigation

--- components/nav.js
+++ components/nav.js
@@ -1,12 +1,17 @@
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { useRouter } from 'next/router';
+import ButtonBack from './ButtonBack';
 import LinkIcon from './LinkIcon';
 
 export default function Nav() {
+  const router = useRouter();
+
   return (
     <nav>
       <ul className="flex items-center justify-between p-8">
         <li>
-          <LinkIcon href="/" title="home" icon={faHome} />
+          {router.pathname === '/primary' && <LinkIcon href="/" title="home" icon={faHome} />}
+          {router.pathname !== '/primary' && <ButtonBack />}
         </li>
       </ul>
     </nav>
