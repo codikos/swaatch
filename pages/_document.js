a45b13c82d80d297ecea027843939b2e05419369
Codikos <joel.matelli@codikos.io>
2021-01-19 20:46:06 +0100

    feat: add analytics tracker (#38)

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,26 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const isProduction = process.env.NODE_ENV === 'production';
+    return { ...initialProps, isProduction };
   }
 
   render() {
+    const { isProduction } = this.props;
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {isProduction && (
+            <>
+              <script
+                async
+                src="https://ackee-xi-woad.vercel.app/tracker.js"
+                data-ackee-server="https://ackee-xi-woad.vercel.app"
+                data-ackee-domain-id="dc40b783-d5d2-4b45-9e57-ea6043be27fb"
+              ></script>
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
