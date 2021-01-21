eee5b7c3490e84dcd7c1e753afbe995958cb4216
Codikos <joel.matelli@codikos.io>
2021-01-22 00:58:16 +0100

    Feat logo (#39)
    * feat: add favicon + illustrations for later use
    
    * feat: add logo in home and banner
    
    * feat: better nav and include nav in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+
+import Nav from '@components/Nav';
 
 export default function IndexPage() {
+  const { theme } = useTheme();
+  const imageSrc = `/1x/logo-full-${theme}.png`;
   return (
     <div>
       <Head>
@@ -11,12 +17,16 @@ export default function IndexPage() {
           content="Swaatch is an application that allows users to easily generate color palettes for their design systems. It can calculate the best contrasts based on WCAG recommendations."
         />
       </Head>
+      <Nav />
       <div className="flex flex-col h-screen p-10 bg-white dark:bg-gray-900 place-content-center">
-        <h1 className="p-5 mx-auto text-5xl font-bold text-center text-transparent w-max bg-clip-text bg-gradient-to-r from-pink-500 to-yellow-500">
-          Swaatch!
-        </h1>
-        <h2 className="mt-5 text-2xl text-center">Generate accessible color palettes easily.</h2>
-        <div className="flex flex-row my-10 place-content-center">
+        <div className="mx-auto">
+          <Image src={imageSrc} layout="intrinsic" width={509} height={150} alt="Swaatch" />
+        </div>
+        <h2 className="mt-10 text-2xl text-center">
+          Easily generate accessible color palettes that follows{' '}
+          <abbr title="Web Content Accessibility Guidelines">WCAG</abbr> recommendations.
+        </h2>
+        <div className="flex flex-row mt-10 place-content-center">
           <Link href="/primary">
             <a className="btn-blue">Let's begin!</a>
           </Link>
